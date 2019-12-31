6ffa50ee175e268ac1cabda209bad314f9f1c96a
zhuairen <184950675@qq.com>
2019-12-31 17:28:49 +0800

    commit word count

--- src/main/java/batch/BatchWordCountDemo.java
+++ src/main/java/batch/BatchWordCountDemo.java
@@ -1,9 +1,41 @@
 package batch;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+
+/**
+ * word count demo
+ */
 public class BatchWordCountDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        //获取环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        //读取数据
+        DataSet<String> ds = env.fromElements("flink flink flink","spark spark spark");
+
+        //单词的词频统计
+        DataSet<Tuple2<String, Integer>> sum = ds.flatMap(new LineSplitter())
+                .groupBy(0)
+                .sum(1);
+
+        sum.print();
 
 
+    }
+
+    public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String,Integer>> {
+        @Override
+        public void flatMap(String line, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for(String word:line.split(" ")){
+                out.collect(new Tuple2<>(word,1));
+            }
+        }
     }
 }
